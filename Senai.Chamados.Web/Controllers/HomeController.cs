da0940391ef4a1c46881eef06e703920b3faa276
FIC <FIC@lab_08_ws15.info.local>
2018-06-20 19:28:50 -0300

    Adicionar arquivos de projeto.

---
+++ Senai.Chamados.Web/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Senai.Chamados.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Mensagem = "Bem Vindo";
+            return View();
+        }
+    }
+}
