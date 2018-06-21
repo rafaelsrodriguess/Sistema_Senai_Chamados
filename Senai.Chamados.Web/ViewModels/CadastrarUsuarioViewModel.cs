f7ee91f2f70573cb9c1afa1774862d97e58c12bb
Rafael Rodrigues <rafaelsrodriguess@gmail.com>
2018-06-20 21:49:41 -0300

    Alteração do Layout

--- Senai.Chamados.Web/ViewModels/CadastrarUsuarioViewModel.cs
+++ Senai.Chamados.Web/ViewModels/CadastrarUsuarioViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Senai.Chamados.Web.Models
+namespace Senai.Chamados.Web.ViewModels
 {
     public class CadastrarUsuarioViewModel
     {
