3d3eccbd0b060673fd5b5ca2c9b9f88d07f4ae12
Rafael Rodrigues <rafaelsrodriguess@gmail.com>
2018-06-21 21:44:47 -0300

    Tela de Cadastro de Usuario.

--- Senai.Chamados.Web/Controllers/ContaController.cs
+++ Senai.Chamados.Web/Controllers/ContaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Senai.Chamados.Web.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,13 +10,33 @@ namespace Senai.Chamados.Web.Controllers
     public class ContaController : Controller
     {
         // GET: Conta
+        [HttpGet]
         public ActionResult Login()
         {
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel usuario)
+        {
+
+            return View();
+        }
+        [HttpGet]
         public ActionResult CadastrarUsuario()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CadastrarUsuario(CadastrarUsuarioViewModel usuario)
+        {
+            if(!ModelState.IsValid)
+            {
+                ViewBag.Erro = "Dados invalidos";
+                return View();
+            }
+            //TODO:Efettuar cadastro no banco de dados
             return View();
         }
     }
--- Senai.Chamados.Web/ViewModels/LoginViewModel.cs
+++ Senai.Chamados.Web/ViewModels/LoginViewModel.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+
 namespace Senai.Chamados.Web.ViewModels
 {
-    public class CadastrarUsuarioViewModel
+    public class LoginViewModel
     {
-        [Display(Name = "Informe o nome")]
-        [Required(ErrorMessage ="Informe o campo nome")]
-        public string Nome {get; set; }
 
         [Display(Name = "Informe o Email")]
         [Required(ErrorMessage = "Informe o campo email")]
@@ -17,15 +15,9 @@ namespace Senai.Chamados.Web.ViewModels
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Display(Name = "Informe o Telefone")]
-        [Required(ErrorMessage = "Informe o campo Telefone")]
-        [DataType(DataType.PhoneNumber)]
-        public string Telefone { get; set; }
-
         [Display(Name = "Informe a Senha")]
         [Required(ErrorMessage = "Informe a senha Senha")]
         [DataType(DataType.Password)]
         public string Senha { get; set; }
-        
     }
 }
